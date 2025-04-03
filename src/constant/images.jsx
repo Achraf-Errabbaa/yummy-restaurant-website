48b85ab020c8997fe6eda0524d88b7e30ec49d9e
achraf errabbaa <achraferrabbaa1@gmail.com>
2025-04-03 16:30:49 +0000

    s

--- src/constant/images.jsx
+++ src/constant/images.jsx
@@ -1,55 +1,55 @@
 // Import static images directly
-import hero from "/src/assets/images/hero-img.png";
-import about from "/src/assets/images/about.jpg";
-import about2 from "/src/assets/images/about-2.jpg";
-import stats from "/src/assets/images/stats-bg.jpg";
+import hero from "../assets/images/hero-img.png";
+import about from "../assets/images/about.jpg";
+import about2 from "../assets/images/about-2.jpg";
+import stats from "../assets/images/stats-bg.jpg";
 
 // Menu items
-import affogato from "/src/assets/images/menu/affogato.png";
-import antipastoPlatter from "/src/assets/images/menu/antipastoPlatter.png";
-import arancini from "/src/assets/images/menu/arancini.png";
-import bruschetta from "/src/assets/images/menu/bruschetta.png";
-import cannoli from "/src/assets/images/menu/cannoli.png";
-import capreseSalad from "/src/assets/images/menu/capreseSalad.png";
-import eggplantParmesan from "/src/assets/images/menu/eggplant-parmesan.png";
-import gelatoTrio from "/src/assets/images/menu/gelatoTrio.png";
-import lasagnabolognese from "/src/assets/images/menu/lasagna-bolognese.png";
-import ossoBuco from "/src/assets/images/menu/osso-buco.png";
-import pannaCotta from "/src/assets/images/menu/panna-cotta.png";
-import risotto from "/src/assets/images/menu/risotto-alla-milanese.png";
-import espresso from "/src/assets/images/menu/espresso.png";
-import negroni from "/src/assets/images/menu/negroni.png";
-import limoncello from "/src/assets/images/menu/limoncello.png";
-import pellegrino from "/src/assets/images/menu/pellegrino.png";
+import affogato from "../assets/images/menu/affogato.png";
+import antipastoPlatter from "../assets/images/menu/antipastoPlatter.png";
+import arancini from "../assets/images/menu/arancini.png";
+import bruschetta from "../assets/images/menu/bruschetta.png";
+import cannoli from "../assets/images/menu/cannoli.png";
+import capreseSalad from "../assets/images/menu/capreseSalad.png";
+import eggplantParmesan from "../assets/images/menu/eggplant-parmesan.png";
+import gelatoTrio from "../assets/images/menu/gelatoTrio.png";
+import lasagnabolognese from "../assets/images/menu/lasagna-bolognese.png";
+import ossoBuco from "../assets/images/menu/osso-buco.png";
+import pannaCotta from "../assets/images/menu/panna-cotta.png";
+import risotto from "../assets/images/menu/risotto-alla-milanese.png";
+import espresso from "../assets/images/menu/espresso.png";
+import negroni from "../assets/images/menu/negroni.png";
+import limoncello from "../assets/images/menu/limoncello.png";
+import pellegrino from "../assets/images/menu/pellegrino.png";
 
 // Testimonials
-import testimonials1 from "/src/assets/images/testimonials/testimonials-1.jpg";
-import testimonials2 from "/src/assets/images/testimonials/testimonials-2.jpg";
-import testimonials3 from "/src/assets/images/testimonials/testimonials-3.jpg";
-import testimonials4 from "/src/assets/images/testimonials/testimonials-4.jpg";
+import testimonials1 from "../assets/images/testimonials/testimonials-1.jpg";
+import testimonials2 from "../assets/images/testimonials/testimonials-2.jpg";
+import testimonials3 from "../assets/images/testimonials/testimonials-3.jpg";
+import testimonials4 from "../assets/images/testimonials/testimonials-4.jpg";
 
 // Team and chefs
-import teamShape from "/src/assets/images/team-shape.svg";
-import chef1 from "/src/assets/images/chefs/chefs-1.jpg";
-import chef2 from "/src/assets/images/chefs/chefs-2.jpg";
-import chef3 from "/src/assets/images/chefs/chefs-3.jpg";
+import teamShape from "../assets/images/team-shape.svg";
+import chef1 from "../assets/images/chefs/chefs-1.jpg";
+import chef2 from "../assets/images/chefs/chefs-2.jpg";
+import chef3 from "../assets/images/chefs/chefs-3.jpg";
 
 // Events
-import reservation from "/src/assets/images/reservation.jpg";
-import event1 from "/src/assets/images/events-1.jpg";
-import event2 from "/src/assets/images/events-2.jpg";
-import event3 from "/src/assets/images/events-3.jpg";
-import event4 from "/src/assets/images/events-4.jpg";
+import reservation from "../assets/images/reservation.jpg";
+import event1 from "../assets/images/events-1.jpg";
+import event2 from "../assets/images/events-2.jpg";
+import event3 from "../assets/images/events-3.jpg";
+import event4 from "../assets/images/events-4.jpg";
 
 // Gallery
-import gallery1 from "/src/assets/images/gallery/gallery-1.jpg";
-import gallery2 from "/src/assets/images/gallery/gallery-2.jpg";
-import gallery3 from "/src/assets/images/gallery/gallery-3.jpg";
-import gallery4 from "/src/assets/images/gallery/gallery-4.jpg";
-import gallery5 from "/src/assets/images/gallery/gallery-5.jpg";
-import gallery6 from "/src/assets/images/gallery/gallery-6.jpg";
-import gallery7 from "/src/assets/images/gallery/gallery-7.jpg";
-import gallery8 from "/src/assets/images/gallery/gallery-8.jpg";
+import gallery1 from "../assets/images/gallery/gallery-1.jpg";
+import gallery2 from "../assets/images/gallery/gallery-2.jpg";
+import gallery3 from "../assets/images/gallery/gallery-3.jpg";
+import gallery4 from "../assets/images/gallery/gallery-4.jpg";
+import gallery5 from "../assets/images/gallery/gallery-5.jpg";
+import gallery6 from "../assets/images/gallery/gallery-6.jpg";
+import gallery7 from "../assets/images/gallery/gallery-7.jpg";
+import gallery8 from "../assets/images/gallery/gallery-8.jpg";
 
 export const images = {
     hero,
